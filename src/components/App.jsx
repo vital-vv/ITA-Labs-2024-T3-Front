f94ddf07cc824064f8a38f8a344b2c36a53d0829
Amatid <Dimagrodno@yandex.ru>
2024-02-01 18:10:10 +0300

    Fixed names and stick footer to bottom of page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,20 @@
 import '../assets/styles/nullStyles.module.css';
 import { AppRoutes } from './Routes/AppRoutes.jsx';
 import { Header } from './Header/Header.jsx';
-import LotsList from './LotsList/LotsList';
+import LotsList from './LotsList/LotsList.jsx';
 import Footer from './Footer/Footer';
-import LotView from './LotView/LotView.jsx'
+import LotView from './LotView/LotView.jsx';
+import classes from '../assets/styles/nullStyles.module.css';
 
 function App() {
   return (
     <div>
+      <div className={classes.main}>
       <Header />
-      {/* <LotView /> */}
-      {/* <AppRoutes /> */}
-      {/* <LotsList />  */}
+        {/* <LotView /> */}
+        {/* <AppRoutes /> */}
+        {/* <LotsList />  */}
+      </div>
       <Footer />
     </div>
   );
