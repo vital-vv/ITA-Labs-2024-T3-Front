78f4bf2d61a41839d128cfe87204f8bfc4224d5e
nastoika <nastya.gorlachyova@mail.ru@>
2024-02-09 12:53:31 +0300

    separated component

--- src/components/UserData/UserDataInputs/UserDataInputs.jsx
+++ src/components/UserData/UserDataInputs/UserDataInputs.jsx
@@ -1,10 +1,12 @@
 import styles from './UserDataInputs.module.scss';
+import {TextField} from '@mui/material';
+
 import {NavLink} from 'react-router-dom';
 import {ROUTES} from '../../../utils/routes.js';
-import {TextField} from '@mui/material';
 import PropTypes, {oneOfType,string, element} from 'prop-types';
-import editIcon from '../../../assets/images/editIconGreen.png';
-import deleteIcon from '../../../assets/images/deleteIcon.png';
+
+import {ButtonGroup} from '../ButtonGroup/ButtonGroup.jsx';
+
 
 UserDataInputs.propTypes = {
     title: oneOfType([string, element]),
@@ -12,25 +14,6 @@ UserDataInputs.propTypes = {
 };
 
 function UserDataInputs({display, title}) {
-
-    function ButtonGroup() {
-        if (display === 'create') {
-            return <button className={`${styles.btn} ${styles.greenBtn}`} disabled={true}>Create user</button>
-        } else if (display === 'edit') {
-            return (
-                <>
-                    <button className={`${styles.btn} ${styles.greenBtn}`}>
-                        <img src={editIcon} alt={editIcon}/>
-                        <p>Edit data</p>
-                    </button>
-                    <button className={`${styles.btn} ${styles.redBtn}`}>
-                        <img src={deleteIcon} alt={deleteIcon}/>
-                        <p>Delete user</p>
-                    </button>
-                </>
-            )
-        }
-    }
 
     return (
         <div className={styles.userBox}>
@@ -54,7 +37,7 @@ function UserDataInputs({display, title}) {
                         <TextField disabled = {display !== 'create'} label="Phone" className={styles.phoneNumber}/>
                     </div>
                     <div className={styles.groupBtn}>
-                        <ButtonGroup/>
+                        <ButtonGroup display={display}/>
                     </div>
                 </div>
             </div>
