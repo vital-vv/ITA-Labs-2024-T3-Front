102ceb268e128ff3120456b65e3e23ffc40850e5
Amatid <Dimagrodno@yandex.ru>
2024-02-28 15:03:46 +0300

    Add nested routes for components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,11 @@
 import '../assets/styles/nullStyles.module.css';
 import { AppRoutes } from './Routes/AppRoutes.jsx';
-import { Header } from './Header/Header.jsx';
-import Footer from './Footer/Footer';
-import classes from '../assets/styles/nullStyles.module.css';
 
 function App() {
   return (
-    <div>
-      <div className={classes.main}>
-        <Header />
-        <AppRoutes />
-      </div>
-      <Footer />
-    </div>
+    <>
+      <AppRoutes />
+    </>
   );
 }
 
--- src/components/Routes/AppRoutes.jsx
+++ src/components/Routes/AppRoutes.jsx
@@ -1,35 +1,41 @@
-import {Routes, Route} from 'react-router-dom';
-import {HomePage} from '../../views/HomePage/HomePage.jsx';
-import {AdminLayout} from '../../views/AdminLayout/AdminLayout.jsx';
-import {UserTable} from '../TableOfUsers/UserTable.jsx';
-import {UserEditor} from '../UserData/UserEditor/UserEditor.jsx';
-import {AllBets} from '../AllBets/AllBets.jsx';
+import { Routes, Route } from 'react-router-dom';
+import { HomePage } from '../../views/HomePage/HomePage.jsx';
+import { AdminLayout } from '../../views/AdminLayout/AdminLayout.jsx';
+import { UserTable } from '../TableOfUsers/UserTable.jsx';
+import { UserEditor } from '../UserData/UserEditor/UserEditor.jsx';
+import { AllBets } from '../AllBets/AllBets.jsx';
 import Account from '../Account/Account.jsx';
 import LotsList from '../Lotslist/Lotslist.jsx';
 import LotView from '../LotView/LotView.jsx';
 import { ROUTES } from '../../utils/routes.js';
-import {UserCreator} from '../UserData/UserCreator/UserCreator.jsx';
+import { UserCreator } from '../UserData/UserCreator/UserCreator.jsx';
 import AddLot from '../AddLot/AddLot.jsx';
 import Preview from '../../hoc/Preview/Preview.jsx';
+import Lots from '../../hoc/Lots/Lots.jsx';
 
 function AppRoutes() {
-
-    return (
-        <Routes>
-    <Route index element={<HomePage/>}/>
-    <Route path={ROUTES.LOTSLIST} element={<LotsList />} />
-    <Route path={ROUTES.LOTVIEW} element={<LotView />} />
-    <Route path='/addlot/' element={<AddLot />} />
-    <Route path='/addlot/preview' element={<Preview />} />
-    <Route path="/admin/*" element={<AdminLayout/>}>
-        <Route path="users" element={<UserTable/>}/>
-        <Route path="users/create" element={<UserCreator/>}/>
-        <Route path="users/:id" element={<UserEditor/>}/>
-        <Route path="bets" element={<AllBets/>}/>
-        <Route path="account" element={<Account/>}/>
-    </Route>
-</Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<Lots />}>
+        <Route index element={<HomePage />} />
+      </Route>
+      <Route path="lotslist/*" element={<Lots />}>
+        <Route index element={<LotsList />} />
+        <Route path="lotview" element={<LotView />} />
+      </Route>
+      <Route path="/addlot/*" element={<Lots />}>
+        <Route index element={<AddLot />} />
+        <Route path="preview" element={<Preview />} />
+      </Route>
+      <Route path="/admin/*" element={<AdminLayout />}>
+        <Route path="users" element={<UserTable />} />
+        <Route path="users/create" element={<UserCreator />} />
+        <Route path="users/:id" element={<UserEditor />} />
+        <Route path="bets" element={<AllBets />} />
+        <Route path="account" element={<Account />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export { AppRoutes };
