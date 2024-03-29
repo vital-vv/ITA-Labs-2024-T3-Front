10ee8234023d49f1a4d9a07e6aa8069bffa5bfaa
Anastasia Gorlachyova <70635803+nastoika@users.noreply.github.com>
2024-03-29 10:46:28 +0300

    Made reject/approve btns for auction (#67)
    * Made reject/approve btns for auction
    
    Co-authored-by: nastoika <anastasia.gorlachyova@gmail.com>

--- src/components/Goods/Goods.jsx
+++ src/components/Goods/Goods.jsx
@@ -1,108 +1,97 @@
 import classes from './Goods.module.scss';
 import photo from '../../assets/images/apples.png';
 import Clock from '../../assets/svg/Clock';
-import Hammer from '../../assets/svg/Hammer';
-import Cart from '../../assets/svg/Cart';
-import Trash from '../../assets/svg/Trash';
-import { ModalWindow } from './ModalWindow/ModalWindow.jsx';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { changeModalThrough } from '../../features/lots/lotsSlice.js';
+import {ModalWindow} from './ModalWindow/ModalWindow.jsx';
+import {useState} from 'react';
+import {useDispatch} from 'react-redux';
+import {changeModalThrough} from '../../features/lots/lotsSlice.js';
+import {LotButtons} from "./LotButtons/LotButtons.jsx";
 
 function Goods({
-  lotItem,
-  dateCreated,
-  daysRest,
-  hoursRest,
-  buttonDelete,
-  id,
-}) {
-  const [open, setOpen] = useState(false);
-  const [minValue, setMinValue] = useState(0);
-  const [maxValue, setMaxValue] = useState(0);
-  const dispatch = useDispatch();
+                   lotItem,
+                   dateCreated,
+                   daysRest,
+                   hoursRest,
+                   buttonDelete,
+                   id,
+                   userRole,
+               }) {
+    const [open, setOpen] = useState(false);
+    const [minValue, setMinValue] = useState(0);
+    const [maxValue, setMaxValue] = useState(0);
+    const dispatch = useDispatch();
 
-  const toggleModal = (event) => {
-    setOpen((prevOpen) => !prevOpen);
-    if (lotItem.leading) {
-      setMinValue(lotItem.leading.amount + 1);
-    } else {
-      setMinValue(1);
-    }
-    setMaxValue(lotItem.price_per_unit*lotItem.quantity - 1);
-    dispatch(changeModalThrough(event.target.id));
-  };
+    const toggleModal = (event) => {
+        setOpen((prevOpen) => !prevOpen);
+        if (lotItem.leading) {
+            setMinValue(lotItem.leading.amount + 1);
+        } else {
+            setMinValue(1);
+        }
+        setMaxValue(lotItem.price_per_unit * lotItem.quantity - 1);
+        dispatch(changeModalThrough(event.target.id));
+    };
 
-  return (
-    <>
-      <div className={classes.goods}>
-        <div className={classes}>
-          <img src={photo} alt="Photo of goods" />
-        </div>
-        <div className={classes.info}>
-          <div>
-            <p className={classes.labelOfGood}>{lotItem.title}</p>
-            <p className={classes.data}>
-              <p>
+    return (
+        <>
+            <div className={classes.goods}>
+                <div className={classes}>
+                    <img src={photo} alt="Photo of goods"/>
+                </div>
+                <div className={classes.info}>
+                    <div>
+                        <p className={classes.labelOfGood}>{lotItem.title}</p>
+                        <p className={classes.data}>
+                            <p>
                 <span>
-                  <Clock />
+                  <Clock/>
                 </span>
-                <span>
+                                <span>
                   {daysRest}d {hoursRest}h
                 </span>
-              </p>
-              <p>ID{id}</p>
-            </p>
-          </div>
-          <div className={classes.more}>
-            <p className={classes.description}>
-              {`${lotItem.category_name}, ${lotItem.variety}, ${lotItem.quantity} ton, ${lotItem.size} mm, ${lotItem.packaging}`}
-            </p>
-            <p className={classes.region}>
-              {lotItem.location.country}, {lotItem.location.region}
-            </p>
-            <p className={classes.dataOfAd}>{dateCreated}</p>
-          </div>
-        </div>
-        <div className={classes.bet}>
-          <div>
-            <div className={classes.cost}>
-              <p className={!lotItem.leading ? classes.grey : null}>
-                {lotItem.leading ? `$${lotItem.leading.amount}` : 'No bets'}
-              </p>
-              <p className={!lotItem.leading ? classes.hidden : null}>
-                $<span>{lotItem.leading && (lotItem.leading.amount/lotItem.quantity).toFixed(2)}</span>/kg
-              </p>
+                            </p>
+                            <p>ID{id}</p>
+                        </p>
+                    </div>
+                    <div className={classes.more}>
+                        <p className={classes.description}>
+                            {`${lotItem.category_name}, ${lotItem.variety}, ${lotItem.quantity} ton, ${lotItem.size} mm, ${lotItem.packaging}`}
+                        </p>
+                        <p className={classes.region}>
+                            {lotItem.location.country}, {lotItem.location.region}
+                        </p>
+                        <p className={classes.dataOfAd}>{dateCreated}</p>
+                    </div>
+                </div>
+                <div className={classes.bet}>
+                    <div>
+                        <div className={classes.cost}>
+                            <p className={!lotItem.leading ? classes.grey : null}>
+                                {lotItem.leading ? `$${lotItem.leading.amount}` : 'No bets'}
+                            </p>
+                            <p className={!lotItem.leading ? classes.hidden : null}>
+                                $<span>{lotItem.leading && (lotItem.leading.amount / lotItem.quantity).toFixed(2)}</span>/kg
+                            </p>
+                        </div>
+                        <div className={classes.perKg}>
+                            {/* Still hardcode */}
+                            <p>${lotItem.price_per_unit * lotItem.quantity}</p>
+                            <p>
+                                <span>${lotItem.price_per_unit}</span>/{lotItem.weight}
+                            </p>
+                        </div>
+                    </div>
+                    <div
+                        className={classes.purchasing}
+                        onClick={(event) => event.preventDefault()}
+                    >
+                        <LotButtons userRole={userRole} id={id} title={lotItem.title} buttonDelete={buttonDelete}/>
+                    </div>
+                </div>
             </div>
-            <div className={classes.perKg}>
-              {/* Still hardcode */}
-              <p>${lotItem.price_per_unit*lotItem.quantity}</p>
-              <p>
-                <span>${lotItem.price_per_unit}</span>/{lotItem.weight}
-              </p>
-            </div>
-          </div>
-          <div
-            className={classes.purchasing}
-            onClick={(event) => event.preventDefault()}
-          >
-            <button onClick={toggleModal} id={id}>
-              <Hammer />
-              My bet
-            </button>
-            <button>
-              <Cart />
-              Buy now
-            </button>
-            <button onClick={buttonDelete} id={id}>
-              <Trash />
-            </button>
-          </div>
-        </div>
-      </div>
-      <ModalWindow open={open} handleClose={toggleModal} minValue={minValue} maxValue={maxValue}/>
-    </>
-  );
+            <ModalWindow open={open} handleClose={toggleModal} minValue={minValue} maxValue={maxValue}/>
+        </>
+    );
 }
 
 export default Goods;
---
+++ src/components/Goods/LotButtons/ModalForReject/ModalForReject.jsx
@@ -0,0 +1,74 @@
+import {Modal, Snackbar, TextField} from "@mui/material";
+import {Box} from "@mui/system";
+import styles from './ModalForRejecr.module.scss';
+import {useState} from "react";
+import {useDispatch} from "react-redux";
+import {rejectLot} from "../../../../features/filter/filterSlice.js";
+
+const ModalForReject = ({handleClose, open, id, title}) => {
+
+    const [description, setDescription] = useState('');
+    const [error, setError] = useState(false);
+    const dispatch = useDispatch();
+
+    const [openSnackBar, setOpenSnackBar] = useState(false);
+    const handleOpenSnackBar = () => {
+        setOpenSnackBar(true);
+    };
+
+    const handleCloseSnackBar = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpenSnackBar(false);
+    };
+
+    function handleRejectLot() {
+        if (description.trim() === '') {
+            setError(true);
+            return;
+        }
+        setError(false);
+        dispatch(rejectLot({id, description}));
+        handleOpenSnackBar();
+        handleClose();
+        console.log(openSnackBar);
+    }
+
+    return (
+        <>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box className={styles.modalContainer}>
+                    <div className={styles.rejectTitle}>Lot <span className={styles.rejectSpan}>{title}</span> will be
+                        rejected because...
+                    </div>
+                    <TextField
+                        id="outlined-multiline-static"
+                        label="Description"
+                        multiline
+                        rows={4}
+                        error={error}
+                        required={true}
+                        value={description}
+                        helperText={error ? "Description cannot be empty" : ""}
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                    <button className={styles.rejectBtn} onClick={handleRejectLot} id={id}>Reject lot</button>
+                </Box>
+            </Modal>
+            <Snackbar
+                open={openSnackBar}
+                autoHideDuration={5000}
+                onClose={handleCloseSnackBar}
+                message="This Snackbar will be dismissed in 5 seconds."
+            />
+        </>
+    );
+};
+
+export {ModalForReject};
