2bcae11f9a44d25bf86c66a533a72416b1cba17f
Anastasia Gorlachyova <70635803+nastoika@users.noreply.github.com>
2024-04-08 10:07:26 +0300

    hot fix config (#87)
    Co-authored-by: nastoika <anastasia.gorlachyova@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/team3/',
-  plugins: [react()],
+    build: {
+        target: 'esnext'
+    },
+    base: '/team3/',
+    plugins: [react()],
 })
